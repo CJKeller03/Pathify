ed4b46a05180959a0ef4d8fa71fd1c7c7ea99eeb
Caleb Keller <caleb@calebjkeller.com>
2020-06-24 15:46:10 -0400

    Fixed locationHandling package.

--- src/main/java/com/calebjkeller/locationHandling/Location.java
+++ src/main/java/com/calebjkeller/locationHandling/Location.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package locationHandling;
+package com.calebjkeller.locationHandling;
 
 /**
  *
